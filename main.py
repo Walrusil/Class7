dd9a0fc4ff408e02111f1cd50f0998a3f994d057
Walrusil <liran.silver@gmail.com>
2025-02-25 19:12:43 +0200

    5th commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("This is a third code change!")
+    print("This is a forth code change!")
 
 
 # Press the green button in the gutter to run the script.
